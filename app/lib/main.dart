a60a79cd31ebf58592e1765f0d07719c7f89c7ab
Rafael David <rafaelcapitaodavid@gmail.com>
2024-04-20 01:00:31 +0100

    foda se

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -5,7 +5,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(
     child: MyApp(),
   ));
